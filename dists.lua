209864ee2b418f490fac3ae5e8dbd10066fbcae1
Darkknight900 <dark900@xyz.de>
2011-03-09 15:11:13 +0100

    dists

--- dists.lua
+++ dists.lua
@@ -35,7 +35,7 @@ hydro[i]=is_hydrophobic(i)
 end
 end
 
-function bandmaxdist() --by Rav3n_pl based on Tlaloc`s
+function bandmaxdist()
 	distances={}
 	distances=getdistances()
     local maxdistance=0
@@ -59,11 +59,13 @@ function bandmaxdist() --by Rav3n_pl based on Tlaloc`s
 	until maxdistance<=20
 	band_set_strength(get_band_count(),maxdistance/15)
 	band_set_length(get_band_count(),maxdistance)
+end
 
-	for x=1, numsegs - 2 do
+function push()
+for x=1, numsegs - 2 do
         if not hydro[x] then
             for y = x + 2, numsegs do
-                if (not hydro[y]) and (math.random() <= 0.003) then
+                if (not hydro[y]) then
 				maxdistance = distances[x][y]
                 band_add_segment_segment(x, y)
 				repeat
@@ -76,15 +78,12 @@ function bandmaxdist() --by Rav3n_pl based on Tlaloc`s
             end
         end
     end
-end
-
-function push()
     distances={}
 	distances=getdistances()
     for x=1, numsegs - 2 do
         if not hydro[x] then
             for y = x + 2, numsegs do
-                if (not hydro[y]) and (math.random() <= ((numsegs/(get_band_count()+1))*0.01)) then
+                if (not hydro[y])then
                     local distance = distances[x][y]
                     if  distance <= 15 then
                         band_add_segment_segment(x, y)
@@ -133,85 +132,94 @@ function CreateBandsToCenter()
    end
 end
 
-function fgain()
-    set_behavior_clash_importance(1)
-    repeat
-        local iter = 0
-        repeat
-            iter = iter + 1
-            local s1 = get_score(true)
-            do_global_wiggle_all(iter)
-            local s2 = get_score(true)
-        until s2 - s1 < 0.01
-        local s3 = get_score(true)
+function fstruct(g, cl)
+    set_behavior_clash_importance(cl)
+    if g == "s" then
         do_shake(1)
-        local s4 = get_score(true)
-    until s4 - s3 < 0.001
+    elseif g == "w" then
+        do_global_wiggle_all(1)
+    end
 end
 
 function floss(option, cl1, cl2)
-    print("Fuzing Method ", option)
+    p("Fuzing Method ", option)
+    p("cl1 ", cl1, ", cl2 ", cl2)
     if option == 1 then
-        print("Pink Fuse cl1-s-cl2-wa: cl1 ", cl1, ", cl2 ", cl2)
-        set_behavior_clash_importance(cl1)
-        do_shake(1)
-        set_behavior_clash_importance(cl2)
-        do_global_wiggle_all(1)
+        p("Pink Fuse cl1-s-cl2-wa")
+        fstruct("s", cl1)
+        fstruct("w", cl2)
     elseif option == 2 then
-        print("Pink Fuse cl1-wa-cl=1-wa-cl2-wa: cl1 ", cl1, ", cl2 ", cl2)
-        set_behavior_clash_importance(cl1)
-        do_global_wiggle_all(1)
-        set_behavior_clash_importance(1)
-        do_global_wiggle_all(1)
-        set_behavior_clash_importance(cl2)
-        do_global_wiggle_all(1)
+        p("Pink Fuse cl1-wa-cl=1-wa-cl2-wa")
+        fstruct("w", cl1)
+        fstruct("w", 1)
+        fstruct("w", cl2)
     elseif option == 3 then
-        print("cl1-s; cl2-s;: cl1 ", cl1, ", cl2 ", cl2)
-        set_behavior_clash_importance(cl1)
-        do_shake(1)
-        fgain()
-        set_behavior_clash_importance(cl2)
-        do_shake(1)
+        p("Blue Fuse cl1-s; cl2-s;")
+        fstruct("s", cl1)
+        gain()
+        fstruct("s", cl2)
     elseif option == 4 then
-        print("cl1-wa[-cl2-wa]: cl1 ", cl1, "[, cl2 ", cl2, "]")
-        set_behavior_clash_importance(cl1)
-        do_global_wiggle_all(1)
-        set_behavior_clash_importance(cl2)
-        do_global_wiggle_all(1)
+        p("cl1-wa[-cl2-wa]")
+        fstruct("w", cl1)
+        fstruct("w", cl2)
     elseif option == 5 then
-        print("qStab cl1-s-cl2-wa-cl=1-s: cl1 ", cl1, ", cl2 ", cl2)
-        set_behavior_clash_importance(cl1)
-        do_shake(1)
-        set_behavior_clash_importance(cl2)
-        do_global_wiggle_all(1)
-        set_behavior_clash_importance(1)
-        do_shake(1)
+        p("qStab cl1-s-cl2-wa-cl=1-s")
+        fstruct("s", cl1)
+        fstruct("w", cl2)
+        fstruct("s", 1)
     end
 end
 
 function s_fuze(option, cl1, cl2)
-    local s1 = get_score(true)
+    local s1_f = get_score(true)
     floss(option, cl1, cl2)
-    fgain()
-    local s2 = get_score(true)
-    if s2 > s1 then
-        reset_recent_best()
-        print("+", s2 - s1)
+    gain()
+    local s2_f = get_score(true)
+    if s2_f > s1_f then
+        if fastfuze then
+            quicksave(sl_f[1])
+        else
+            sl_f[#sl_f + 1] = RequestSaveSlot()
+            quicksave(sl_f[#sl_f])
+        end
+        p("+", s2_f - s1_f, "+")
     end
-    restore_recent_best()
+    quickload(sl_f[1])
 end
 
 function fuze(sl)
-    select_all()
-    reset_recent_best()
-    s_fuze(1, 0.1, 0.7)
-    s_fuze(1, 0.3, 0.6)
-    s_fuze(2, 0.5, 0.7)
-    s_fuze(2, 0.7, 0.5)
-    s_fuze(3, 0.05, 0.07)
-    s_fuze(4, 0.3, 0.3)
-    s_fuze(5, 0.1, 0.4)
+    if b_fuze then
+        select_all()
+        sl_f = {}
+        sl_f[1] = RequestSaveSlot()
+        quicksave(sl_f[1])
+        s_fuze(1, 0.1, 0.7)
+        s_fuze(1, 0.3, 0.6)
+        s_fuze(2, 0.5, 0.7)
+        s_fuze(2, 0.7, 0.5)
+        s_fuze(3, 0.05, 0.07)
+        s_fuze(4, 0.3, 0.3)
+        s_fuze(5, 0.1, 0.4)
+        local s_f = get_score()
+        if not fastfuze then
+            for i = 2, #sl_f do
+                quickload(sl_f[i])
+                s_f1 = get_score(true)
+                if s_f1 > s_f then
+                    quicksave(sl_f[1])
+                    s_f = s_f1
+                end
+            end
+        end
+        quickload(sl_f[1])
+        if not fastfuze then
+            for i = 1, #sl_f do
+                ReleaseSaveSlot(sl_f[i])
+            end
+        end
+    end
 end
+--Fuzing#
 
 check_hydro()
 for i=0,numsegs do
@@ -227,24 +235,6 @@ sc2=get_score(true)
 if cs<sc2 then reset_recent_best() end
 set_behavior_clash_importance(0.01)
 do_shake(1)
-_CreateHydrophobeBands()
-set_behavior_clash_importance(0.5)
-do_global_wiggle_backbone(1)
-band_delete()
-cs=get_score(true)
-fuze()
-sc2=get_score(true)
-if cs<sc2 then reset_recent_best() end
-set_behavior_clash_importance(0.01)
-do_shake(1)
-push()
-set_behavior_clash_importance(0.5)
-do_global_wiggle_backbone(1)
-band_delete()
-cs=get_score(true)
-fuze()
-sc2=get_score(true)
-if cs<sc2 then reset_recent_best() end
 CreateBandsToCenter()
 set_behavior_clash_importance(0.5)
 do_global_wiggle_backbone(1)
