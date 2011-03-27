be9912c4ffc12723adf9408bcdfbdf49bb9f0a34
Darknight900 <dark900@xyz.de>
2011-03-27 17:58:06 +0200

    lws v1023 *adding bands function copy paste

--- lws.lua
+++ lws.lua
@@ -5,7 +5,7 @@ Special Thanks goes to Gary Forbis for the great description of his Cookbookwork
 ]]
 
 --#Game vars
-Version     = "2.9.1.1022"
+Version     = "2.9.1.1023"
 numsegs     = get_segment_count()
 --Game vars#
 
@@ -985,6 +985,82 @@ range=0
 end
 end
 --Compressor#
+
+--#Bands
+function bands()
+_numsegs=get_segment_count()
+p(_numsegs)
+i=1
+select_all()
+replace_ss("L")
+deselect_all()
+function Round(x)--cut all afer 3-rd place
+	return x-x%1.
+end
+numsegs=Round(_numsegs/2)
+p(numsegs)
+while i<numsegs do
+band_add_segment_segment(numsegs-i,numsegs+i)
+band_set_strength(i,10)
+band_set_length(i,0)
+i=i+1
+end
+band_add_segment_segment(numsegs,_numsegs)
+band_add_segment_segment(numsegs,1)
+bands=get_band_count()
+for i=bands-1,bands do
+band_set_strength(i,10)
+band_set_length(i,0)
+end
+band_add_segment_segment(numsegs/2,_numsegs)
+band_add_segment_segment(numsegs+numsegs/2,_numsegs)
+band_add_segment_segment(numsegs/2,1)
+band_add_segment_segment(numsegs+numsegs/2,1)
+band_add_segment_segment(numsegs/4,numsegs+numsegs/4)
+band_add_segment_segment(numsegs/4,numsegs-numsegs/4)
+band_add_segment_segment(numsegs+numsegs*3/4,numsegs+numsegs/4)
+band_add_segment_segment(numsegs+numsegs*3/4,numsegs-numsegs/4)
+for i=bands+1,bands+8 do
+band_set_strength(i,0.01)
+band_set_length(i,20)
+end
+bands=get_band_count()
+select_all()
+do_global_wiggle_all(1)
+do_shake(1)
+for i=1,bands do
+band_set_strength(i,5)
+band_set_length(i,5)
+end
+do_global_wiggle_backbone(1)
+do_shake(1)
+do_global_wiggle_sidechains(1)
+for i=1,bands do
+band_set_strength(i,2)
+band_set_length(i,5)
+end
+do_global_wiggle_backbone(1)
+do_shake(1)
+do_global_wiggle_sidechains(1)
+for i=1,bands do
+band_set_strength(i,1)
+band_set_length(i,4)
+end
+do_global_wiggle_backbone(1)
+do_shake(1)
+do_global_wiggle_sidechains(1)
+for i=1,bands do
+band_set_strength(i,0.01)
+end
+do_global_wiggle_backbone(1)
+do_shake(1)
+do_global_wiggle_sidechains(1)
+band_delete()
+do_global_wiggle_backbone(1)
+do_shake(1)
+do_global_wiggle_sidechains(1)
+end
+--Bands#
 
 function all()
     if b_pp then
