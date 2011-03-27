afe345cccb01336a23318eb503eed68f8a267eb9
Darknight900 <dark900@xyz.de>
2011-03-27 17:45:01 +0200

    lws v1022 *implemented compressor just copy paste

--- lws.lua
+++ lws.lua
@@ -5,7 +5,7 @@ Special Thanks goes to Gary Forbis for the great description of his Cookbookwork
 ]]
 
 --#Game vars
-Version     = "2.9.1.1021"
+Version     = "2.9.1.1022"
 numsegs     = get_segment_count()
 --Game vars#
 
@@ -948,6 +948,43 @@ function struct_rebuild()
     rebuilding = false
 end
 --struct rebuild#
+
+--#Compressor
+function compress()
+p("Compressing Segment ",seg)
+sphere={}
+range=0
+repeat
+count=0
+range=range+2
+sphere=fsl.GetSphere(seg,range)
+for n=1,#sphere-1 do
+if sphere[n]>seg+range/4 and sphere[n]+1~=sphere[n+1] or sphere[n]<seg-range/4 and sphere[n]+1~=sphere[n+1] then
+count=count+1
+end
+end
+until count>4
+for n=1,#sphere-1 do
+if sphere[n]>seg+range/4 and sphere[n]+1~=sphere[n+1] or sphere[n]<seg-range/4 and sphere[n]+1~=sphere[n+1] then
+band_add_segment_segment(seg,sphere[n])
+local length=get_segment_distance(seg,sphere[n])
+repeat
+length=length*7/8
+until length<=5
+band_set_length(get_band_count(),length)
+band_set_strength(get_band_count(),length/5)
+end
+end
+do_global_wiggle_backbone(1)
+band_delete()
+else
+p("Compressing Segment ",seg,"-",r)
+sphere1={}
+sphere2={}
+range=0
+end
+end
+--Compressor#
 
 function all()
     if b_pp then
